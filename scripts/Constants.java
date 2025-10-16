528e38366061e042483dd70dcaa90491f1686a8d
Andreas Kull <akullpp@gmail.com>
2025-10-16 22:33:15 +0200

    Improve badge size

--- scripts/Constants.java
+++ scripts/Constants.java
@@ -45,9 +45,9 @@ public final class Constants {
   public static final String TABLE_STYLE = "width:100%;table-layout:fixed;border-collapse:collapse;";
   public static final String NAME_CELL_STYLE = "width:20%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;";
   public static final String DESC_CELL_STYLE = "width:60%;word-wrap:break-word;";
-  public static final String BADGE_CELL_STYLE = "width:10%;text-align:center;vertical-align:middle;";
+  public static final String BADGE_CELL_STYLE = "width:10%;text-align:center;vertical-align:middle;min-height:40px;padding:0;";
 
-  // Badge URLs
+  // Badge URLs - Updated to remove labels
   public static final String GITHUB_STARS_BADGE_URL = "https://img.shields.io/github/stars/%s?style=for-the-badge&label=";
   public static final String GITHUB_LAST_COMMIT_BADGE_URL = "https://img.shields.io/github/last-commit/%s?style=for-the-badge&label=";
 
